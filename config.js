f83114fa37623f35e993f8f7a5f807e2f840a40d
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2017-10-26 13:54:15 -0400

    Tutorial 67

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
 // We have to export each object in order to access them separately
 exports.crypto = parsed.crypto;
+exports.discog = parsed.discog;
 exports.expressSession = parsed.expressSession;
 exports.mailgun = parsed.mailgun;
 exports.port = parsed.port;
