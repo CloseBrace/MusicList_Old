7fb09dba54541ee7f4b4e090bc2faa8f9d2cb9ed
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2017-11-16 13:40:33 -0500

    Tutorial 72 fix

--- models/artist.js
+++ models/artist.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Artist = new Schema({
-  namevariations: [String],
-  profile: String,
-  urls: [String],
   discogsId: { type: Number, unique: true },
   images: [Schema.Types.Mixed],
   members: [Schema.Types.Mixed],
+  name: String,
+  namevariations: [String],
+  profile: String,
+  urls: [String],
 });
 
 module.exports = mongoose.model('Artist', Artist);
